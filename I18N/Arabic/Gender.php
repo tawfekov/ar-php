66863ce08f70af176752c2a133516178c6221bc5
kshamaa <kshamaa@8fcf9fe5-4f1e-4b76-bd7c-3f9259ae38da>
2012-02-19 05:42:54 +0000

    More accurate gender detection by add some formats like إفتعال and إفعال as well as check the list of most common irregular Arabic female names
    git-svn-id: https://ar-php.svn.sourceforge.net/svnroot/ar-php@4 8fcf9fe5-4f1e-4b76-bd7c-3f9259ae38da

--- I18N/Arabic/Gender.php
+++ I18N/Arabic/Gender.php
@@ -120,15 +120,28 @@ class I18N_Arabic_Gender
         
         $words = explode(' ', $str);
         $str   = $words[0];
+		
+		$str = str_replace(array('أ','إ','آ'), 'ا', $str);
         
-        $last      = mb_substr($str, -1, 1, 'UTF-8');
+        $last       = mb_substr($str, -1, 1, 'UTF-8');
         $beforeLast = mb_substr($str, -2, 1, 'UTF-8');
 
         if ($last == 'ة' || $last == 'ه' || $last == 'ى' || 
             $last == 'ا' || ($last == 'ء' && $beforeLast == 'ا')) {
 
             $female = true;
-        }
+        } elseif (preg_match("/^[اإ].{2}ا.$/u", $str) || preg_match("/^[إا].ت.ا.+$/u", $str)) {
+			// الأسماء على وزن إفتعال و إفعال
+			$female = true;
+		} else {
+			// List of the most common irregular Arabic female names
+			$names = file(dirname(__FILE__).'/data/female.txt');
+			$names = array_map('trim', $names);
+			
+			if (array_search($str, $names) > 0) {
+				$female = true;
+			}
+		}
         
         return $female;
     }
