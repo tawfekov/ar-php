1259e2897f8a6cff3e7b8c25060bf8fe80b17914
Tawfek Daghistani <tawfekov@gmail.com>
2012-11-03 10:42:32 +0200

    autoloading classmap

---
+++ autoload_classmap.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    "AutoSummarize"     => __DIR__ . "I18N/Arabic/AutoSummarize",
+    "CharsetC"          => __DIR__ . "I18N/Arabic/CharsetC",
+    "CharsetD"          => __DIR__ . "I18N/Arabic/CharsetD",
+    "CompressStr"       => __DIR__ . "I18N/Arabic/CompressStr",
+    "Date"              => __DIR__ . "I18N/Arabic/Date",
+    "Gender"            => __DIR__ . "I18N/Arabic/Gender",
+    "Glyphs"            => __DIR__ . "I18N/Arabic/Glyphs",
+    "Hiero"             => __DIR__ . "I18N/Arabic/Hiero",
+    "Identifier"        => __DIR__ . "I18N/Arabic/Identifier",
+    "KeySwap"           => __DIR__ . "I18N/Arabic/KeySwap",
+    "Mktime"            => __DIR__ . "I18N/Arabic/Mktime",
+    "Normalise"         => __DIR__ . "I18N/Arabic/Normalise",
+    "Numbers"           => __DIR__ . "I18N/Arabic/Numbers",
+    "Query"             => __DIR__ . "I18N/Arabic/Query",
+    "Salat"             => __DIR__ . "I18N/Arabic/Salat",
+    "Soundex"           => __DIR__ . "I18N/Arabic/Soundex",
+    "Standard"          => __DIR__ . "I18N/Arabic/Standard",
+    "Stemmer"           => __DIR__ . "I18N/Arabic/Stemmer",
+    "Transliteration"   => __DIR__ . "I18N/Arabic/Transliteration",
+    "WordTag"           => __DIR__ . "I18N/Arabic/WordTag",
+    "StrToTime"         => __DIR__ . "I18N/Arabic/StrToTime"
+);
